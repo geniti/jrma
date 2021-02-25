5d835576f9e65b9ffd0194930301a02e7b94fc16
Geniti <geniti.ramautarsing@gmail.com>
2021-02-25 14:00:34 +0100

    Update Card.js

--- components/Card.js
+++ components/Card.js
@@ -7,7 +7,7 @@ function Card() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="lg:text-center">
           <h2 className="text-base text-red-600 font-semibold tracking-wide uppercase">
-            voor al uw afbouw werkzaamheden
+            voor al uw afbouw
           </h2>
           <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
             Heeft u een stuc of spuitwerk klus?
